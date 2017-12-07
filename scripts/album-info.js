3982f9578a11c05932d8fc372c9a086cf94ecd34
Ashley Brown <snowedin.webdesign@gmail.com>
2017-12-06 17:08:40 -0700

    Assignment 11 done

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -3,4 +3,9 @@
   $('#album-title').text(album.title);
   //adds the src attribute to img tag so cover loads
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  //insert artist name into .artist element
+  $('.artist').text(album.artist);
+  //insert release info into #release-info element
+  $('#release-info').text(album.releaseInfo);
+
 }
